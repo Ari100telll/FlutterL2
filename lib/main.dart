e1920b7fce4b6b6f79f44de079a5a313bacf29fc
Nikita Horb <nikita.horb@inoxoft.com>
2021-10-04 22:17:10 +0300

    Shit but working cod of lab2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lab_2/Section1/section1.dart';
 
 void main() {
   runApp(MyApp());
@@ -112,7 +113,7 @@ class _HomePageState extends State<HomePage> {
         // ),
         child: SizedBox(
           width: _elementWidth,
-          child: ListView(
+          child: Column(
             children: [
               _mangaAvatar(),
               _mangaName(),
@@ -122,7 +123,7 @@ class _HomePageState extends State<HomePage> {
       );
     }
 
-    return ListView(
+    return Column(
       children: [
         Container(
           child: ListTile(
@@ -135,19 +136,410 @@ class _HomePageState extends State<HomePage> {
         SizedBox(
           height: 200,
           // width: 500,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              _bigMangaOverview(),
-              _bigMangaOverview(),
-              _bigMangaOverview(),
-              _bigMangaOverview(),
-              _bigMangaOverview(),
-              _bigMangaOverview(),
-              _bigMangaOverview(),
-            ],
+          child: Container(
+            padding: EdgeInsets.only(left: 5.0),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                _bigMangaOverview(),
+                _bigMangaOverview(),
+                _bigMangaOverview(),
+                _bigMangaOverview(),
+                _bigMangaOverview(),
+                _bigMangaOverview(),
+                _bigMangaOverview(),
+              ],
+            ),
           ),
         ),
+      ],
+    );
+  }
+
+  Widget _section2() {
+    Widget _titleSection2() {
+      return Align(
+        alignment: Alignment.topLeft,
+        child: Container(
+          margin: EdgeInsets.only(
+            top: 5.0,
+            left: 13.0,
+          ),
+          child: Text(
+            "Останні новини",
+            textAlign: TextAlign.end,
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      );
+    }
+
+    Widget _newsList() {
+      Widget _news() {
+        Widget _line1() {
+          return Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              margin: EdgeInsets.only(
+                top: 5.0,
+                left: 13.0,
+              ),
+              child: Text(
+                "Течнічні роботи на сервері 15.16.4098",
+                style: TextStyle(color: Colors.white, fontSize: 14),
+              ),
+            ),
+          );
+        }
+
+        Widget _line2() {
+          return Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 15.0,
+              ),
+              child: Wrap(
+                spacing: 20,
+                children: [
+                  Wrap(
+                    spacing: 5,
+                    children: [
+                      Icon(
+                        Icons.account_box,
+                        color: Color(0xFF868e96),
+                        size: 14,
+                      ),
+                      Text(
+                        "username",
+                        style: TextStyle(
+                          color: Color(0xFF868e96),
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "3 days ago",
+                    style: TextStyle(
+                      color: Color(0xFF868e96),
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+
+        return Column(children: [
+          _line1(),
+          _line2(),
+        ]);
+      }
+
+      return Column(
+        children: [
+          _news(),
+          _news(),
+          _news(),
+          _news(),
+        ],
+      );
+    }
+
+    Widget _allNews() {
+      return Align(
+        alignment: Alignment.bottomLeft,
+        child: Container(
+          child: Text(
+            "Усі новини >",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 15.0,
+            bottom: 15,
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        _titleSection2(),
+        _newsList(),
+        _allNews(),
+      ],
+    );
+  }
+
+  Widget _section3() {
+    Widget _titleSection3() {
+      return Align(
+        alignment: Alignment.topLeft,
+        child: Container(
+          margin: EdgeInsets.only(
+            top: 5.0,
+            left: 13.0,
+          ),
+          child: Text(
+            "Останні теми обговорень",
+            textAlign: TextAlign.end,
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      );
+    }
+
+    Widget _discutionsList() {
+      Widget _news() {
+        Widget _line1() {
+          return Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              margin: EdgeInsets.only(
+                top: 5.0,
+                left: 13.0,
+              ),
+              child: Text(
+                "Шукаєм хороших клінерів і переладачів.",
+                style: TextStyle(color: Colors.white, fontSize: 14),
+              ),
+            ),
+          );
+        }
+
+        Widget _line2() {
+          return Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 15.0,
+              ),
+              child: Wrap(
+                spacing: 20,
+                children: [
+                  Wrap(
+                    spacing: 5,
+                    children: [
+                      Icon(
+                        Icons.account_box,
+                        color: Color(0xFF868e96),
+                        size: 14,
+                      ),
+                      Text(
+                        "username",
+                        style: TextStyle(
+                          color: Color(0xFF868e96),
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "3 days ago",
+                    style: TextStyle(
+                      color: Color(0xFF868e96),
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+
+        return Column(children: [
+          _line1(),
+          _line2(),
+        ]);
+      }
+
+      return Column(
+        children: [
+          _news(),
+          _news(),
+          _news(),
+          _news(),
+        ],
+      );
+    }
+
+    Widget _allDiscution() {
+      return Row(
+        children: [
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              child: Text(
+                "Усі теми >",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              padding: EdgeInsets.only(
+                top: 10,
+                left: 15.0,
+                bottom: 15,
+              ),
+            ),
+          ),
+          Spacer(),
+          Container(
+              margin: EdgeInsets.all(10.0),
+              padding: EdgeInsets.only(left: 10, right: 10, top: 2, bottom: 2),
+              decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(
+                      3) // use instead of BorderRadius.all(Radius.circular(20))
+                  ),
+              // color: Colors.purple,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.manage_accounts,
+                    size: 16,
+                  ),
+                  Text(" Допомога перекладачам [ 567 ]"),
+                ],
+              ))
+        ],
+      );
+    }
+
+    return Column(
+      children: [
+        _titleSection3(),
+        _discutionsList(),
+        _allDiscution(),
+      ],
+    );
+  }
+
+  Widget _section4() {
+    Widget _titleSection3() {
+      return Align(
+        alignment: Alignment.topLeft,
+        child: Container(
+          margin: EdgeInsets.only(
+            top: 5.0,
+            left: 13.0,
+          ),
+          child: Text(
+            "Найпопулярніше останнім часом",
+            textAlign: TextAlign.end,
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      );
+    }
+
+    Widget _discutionsList() {
+      Widget _news() {
+        Widget _line1() {
+          return Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              margin: EdgeInsets.only(
+                top: 5.0,
+                left: 13.0,
+              ),
+              child: Text(
+                "Яку мангу варто прочитати?",
+                style: TextStyle(color: Colors.white, fontSize: 14),
+              ),
+            ),
+          );
+        }
+
+        Widget _line2() {
+          return Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 15.0,
+              ),
+              child: Wrap(
+                spacing: 20,
+                children: [
+                  Wrap(
+                    spacing: 5,
+                    children: [
+                      Icon(
+                        Icons.account_box,
+                        color: Color(0xFF868e96),
+                        size: 14,
+                      ),
+                      Text(
+                        "username",
+                        style: TextStyle(
+                          color: Color(0xFF868e96),
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "3 days ago",
+                    style: TextStyle(
+                      color: Color(0xFF868e96),
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+
+        return Column(children: [
+          _line1(),
+          _line2(),
+        ]);
+      }
+
+      return Column(
+        children: [
+          _news(),
+          _news(),
+          _news(),
+          _news(),
+        ],
+      );
+    }
+
+    Widget _allDiscution() {
+      return Row(
+        children: [
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              child: Text(
+                "Усе популярне >",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              padding: EdgeInsets.only(
+                top: 10,
+                left: 15.0,
+                bottom: 15,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Column(
+      children: [
+        _titleSection3(),
+        _discutionsList(),
+        _allDiscution(),
       ],
     );
   }
@@ -158,7 +550,7 @@ class _HomePageState extends State<HomePage> {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(40.0),
         child: AppBar(
-          backgroundColor: Color(0xFF343434),
+          backgroundColor: Color(0xFF1C1C1E),
           bottomOpacity: 0.0,
           elevation: 0.0,
           leading: IconButton(
@@ -166,16 +558,36 @@ class _HomePageState extends State<HomePage> {
           actions: [
             IconButton(
                 onPressed: null,
-                icon: Icon(Icons.monochrome_photos, color: Colors.white)),
+                icon: Icon(Icons.mode_night_outlined, color: Colors.white)),
             IconButton(
                 onPressed: null, icon: Icon(Icons.search, color: Colors.white)),
           ],
         ),
       ),
-      backgroundColor: Color(0xFF343434),
-      body: Column(
-        children: [
-          Expanded(child: _section1()),
+      backgroundColor: Color(0xFF141414),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          Container(
+            color: Color(0xFF1C1C1E),
+            child: _section1(),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            color: Color(0xFF1C1C1E),
+            // height: 250,
+            child: _section2(),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            color: Color(0xFF1C1C1E),
+            child: _section3(),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            color: Color(0xFF1C1C1E),
+            child: _section4(),
+          ),
         ],
       ),
     );
